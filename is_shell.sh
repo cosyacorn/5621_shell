768191c88a5d5495450fd5bc815c877d05975a6c
Shane Harding <sharding@tcd.ie>
2015-02-04 18:47:51 +0000

    started is_shell and lotto

--- is_shell.sh
+++ is_shell.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+FILE=$1
+
+if test -e $FILE; then
+	echo "$FILE exists"
+else
+	echo "$FILE not found"
+	exit 1
+fi
+
+
--- lottery.sh
+++ lottery.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+
+for j in `seq 6`;
+do
+	num1=`expr "1 + ($RANDOM % 45)" | bc`
+	echo "$num1"
+done
+
+
