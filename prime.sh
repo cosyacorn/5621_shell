d1639db9c2b4ee1face8736edfc2bca4ed833f6d
Shane Harding <sharding@tcd.ie>
2015-02-04 16:25:33 +0000

    finished prime and start of quad

--- prime.sh
+++ prime.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-date
+for j in `seq 40001 2 41999`;
+do
+	num=$j
+	sqroot=`expr "sqrt($num)" | bc `
+	#echo "num = $sqroot"
+	i=3
+
+	while [ $i -lt $sqroot ]
+	do
+		if [ `expr $num % $i` -eq 0 ]
+		then
+			break
+		fi
+		i=`expr $i + 2`
+		#echo "$i"
+
+		if [ `expr $sqroot - $i` -eq 0 ]
+		then
+			echo "$num is prime"
+		fi
+	done
+
+done
