2764604b373ff6ba2be6ff83a224ec8968028e62
Shane Harding <sharding@tcd.ie>
2015-02-04 22:19:43 +0000

    finished is_shell

--- is_shell.sh
+++ is_shell.sh
@@ -9,4 +9,13 @@ else
 	exit 1
 fi
 
+count=`grep '#!/bin/bash' $FILE | wc -l`
 
+if [ $count -ne 0 ]
+then
+	echo "The contents of $FILE is:"
+	grep '#!/bin/bash' $FILE
+	grep -v '#' $FILE
+else
+	echo "$FILE is not a shell script"
+fi
