a179533d257369f2c37a753d3a748c1b55133729
Shane Harding <sharding@tcd.ie>
2015-02-06 22:48:16 +0000

    updating decompress and rename

--- decompress.sh
+++ decompress.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+if [ $# -ne 1 ] 
+then
+    echo "Usage: $0 <filename to decompress>"
+    exit
+fi
+
+
+if [ ! -f $1 ]
+then
+  echo "File does not exist"
+  exit
+fi
+
+if [ $1 == *.zip ]
+then
+	unzip $1 > /dev/null
+elif [ $1 == *.gz ]
+then
+	gzip -d $1
+elif [ $1 == *.bz2 ]
+then
+	bzip2 -d $1
+elif [ $1 == *.xz ]
+then
+	xz -d $1
+else
+	echo "File extension not supported"
+fi
--- rename.sh
+++ rename.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+if [ $# -ne 1 ]
+then
+	echo "Correct usage: $0 <directory>"
+	exit
+fi
+
+if [ ! -d $1 ]
+then
+  echo "Directory does not exist"
+  exit
+fi
+
+cd $1	
+`rename 'y/a-z/A-Z/' *`
+cd ..	
