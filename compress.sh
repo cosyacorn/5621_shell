fca1960d0237ffa77c16f6d36eb500b065674099
Shane Harding <sharding@tcd.ie>
2015-02-06 19:59:57 +0000

    finished compress. Updated run_simple_mc cos I realised that i hadn't finished it before

--- compress.sh
+++ compress.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
 
-seq 10
+if [ $# -ne 1 ]
+then
+	echo "Usage $0 <file to compress>"
+	exit
+fi
+
+if [ ! -f $1 ]
+then
+	echo "$1 is not a file"
+	exit
+fi
+
+zip $1.zip $1 > /dev/null
+gzip -c $1 > $1.gz
+bzip2 -c $1 > $1.bz2
+xz -c $1 > $1.xz
+
+
+size=$(stat -c %s $1)
+
+zip_size=$(stat -c %s $1.zip)
+gzip_size=$(stat -c %s $1.gz)
+bzip2_size=$(stat -c %s $1.bz2)
+xz_size=$(stat -c %s $1.xz)
+
+
+echo "Compression rates (frac of original):"
+echo -e "zip: \c"
+echo "scale=2;$zip_size/$size" | bc
+echo -e "gzip: \c"
+echo "scale=2;$gzip_size/$size" | bc
+echo -e "bzip2: \c"
+echo "scale=2;$bzip2_size/$size" | bc
+echo -e "xz: \c"
+echo "scale=2;$xz_size/$size" | bc
+
+
--- run_simple_mc.sh
+++ run_simple_mc.sh
@@ -2,9 +2,23 @@
 
 gcc -o simple_mc simple_mc.c -lm
 
-for i in $(seq 100)
+if [ $# -eq 1 ]
+then
+	runs=$1
+else
+	runs=100
+fi
+
+n_yes=0
+
+for i in $(seq $runs)
 do
-./simple_mc $RANDOM > /dev/null
+	if ./simple_mc $RANDOM | grep y > /dev/null
+	then
+		n_yes=`expr $n_yes + 1`
+	fi
 done
+echo "Fraction of yes answers:"
+echo "scale=2;$n_yes/$runs" | bc 
 
 rm simple_mc
